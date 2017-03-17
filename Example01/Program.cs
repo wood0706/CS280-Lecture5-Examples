f071256d5b0683e069d1cef2ac3e3e12b3a22997
wood0706 <wood0706@gmail.com>
2017-03-17 20:00:10 +0800

    封裝

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -19,25 +19,22 @@ namespace Example01
             {
                 int price = 0;
                 price = int.Parse(Console.ReadLine());
-                if (price > 200)
-                {
-                    product.Price = 200;
-                }
-                else if (price < 39)
-                {
-                    product.Price = 39;
-                }
-                else
-                {
-                    product.Price = price;
-                }
-                Console.WriteLine("產品:{0}的價格是{1}", product.Name, product.Price);
+                product.SetPrice(price);
+                Console.WriteLine("產品:{0}的價格是{1}", product.Name, product.GetPrice());
             }
             catch
             {
                 Console.WriteLine("價格輸入錯誤");
                 return;
             }
+
+            Product product2 = new Product();
+            Console.Write("請輸入產品名稱:");
+            product2.Name = Console.ReadLine();
+            Console.Write("請輸入{0}的價錢:", product2.Name);
+            int price2 = 0;
+            price2 = int.Parse(Console.ReadLine());
+            product2.SetPrice(price2);
         }
     }
 }
--- StoreLibrary/Product.cs
+++ StoreLibrary/Product.cs
@@ -9,7 +9,28 @@ namespace StoreLibrary
     public class Product
     {
         public string Name;
-        public int Price;
+        private int price;
         public float Discount;
+
+        public int GetPrice()
+        {
+            return this.price;
+        }
+
+        public void SetPrice(int price)
+        {
+            if (price > 200)
+            {
+                this.price = 200;
+            }
+            else if (price < 39)
+            {
+                this.price = 39;
+            }
+            else
+            {
+                this.price = price;
+            }
+        }
     }
 }
